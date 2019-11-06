3c780b69b132bd566161afdc19908b544d512c54
JMLayn <jlayn@hashicorp.io>
2019-11-06 17:15:35 -0500

    updated ssh key_data

--- compute/main.tf
+++ compute/main.tf
@@ -29,7 +29,8 @@ resource "azurerm_virtual_machine" "WkstDemo" {
 
     ssh_keys {
       path     = "/home/azureuser/.ssh/authorized_keys"
-      key_data = "${file("~/.ssh/id_rsa.pub")}"
+      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCcHhLORvRvCej6WmChXBtS4nt70zdPZwOOARQ/CbUXcZgZBXhJEtHqpVuXWpi1GSxciGviXVnhqsTwiKGnvapD+ekwSDsX8rpCLQy0zjLwnEPUG284t2ZsuYtmjuQqsFVxo2vjmnLu+J70YAjnMtaWBeI13+G4e9iQOZoBXkUA/rAIyB08uRINuok9GwtoO7lyQu7Q2R4hpQbsduV5wF4Pqdxx2UkUmoXwScXLc4QlW3MvJMahKd8k6/1vNvf/bR8jITYuojoCk5wCKhOGFVZQEY+FzClX6VB3LmavikxgNhaPK3C7RWJXBwHsILGJM6H/xXFIaB3b5ihKdH+XkaFOmMYt8hggrIcyn50YPTzWSutjm48lnGtSN/c5ocA10eRAnu1ArUcrjrLQXPFv9GytlXU8BFvgOIaFBWFYbNR3tBdbB7R+coS6GcAAP18EXV+gyf9Wt+Femfh6F6YL1IAQxXIi7Z85sqoaGMIdNm9ZaafLQX310ZyUhbMtGpLqHace3TLCS3a6685ZbWgvfajITHMQ2T4S+nXvN2rbVeqRaNouCDEPJB+UQKeAfzXrr/OG6ZDb8apSe5MMEPj3z9O49f+vgdQv1dOpKWvNeutO8DeAS+9UiRVD4aC/iBttktiytLAs8vW0YXQoQN/Fc0jsZpwKQeE26cgy3mFqOdjxcQ== jlayn@hashicorp.com"
+      # key_data = "${file("~/.ssh/jmlayn_rsa.pub")}"
     }
   }
 }
