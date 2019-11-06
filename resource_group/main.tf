c5d61bd222ae541608a73684e475ea0bc281e0d0
JMLayn <jlayn@hashicorp.io>
2019-11-06 17:13:44 -0500

    updated tags

--- resource_group/main.tf
+++ resource_group/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "WkstDemo" {
   name     = "${var.resourceGroup}"
   location = "${var.resourceLocation}"
 
-  tags {
+  tags = {
     x-do-not-remove = "${var.x-do-not-remove}"
     X-Contact       = "${var.X-Contact}"
     X-Application   = "${var.X-Application}"
